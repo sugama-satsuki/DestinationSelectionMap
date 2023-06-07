1d3b7936852a0ffbba0d96984013e079e4520454
Higuchi Satsuki <46039732+sugama-satsuki@users.noreply.github.com>
2023-06-07 21:50:44 +0900

    Update sample.js

--- script/sample.js
+++ script/sample.js
@@ -1,47 +1,47 @@
-// mapオブジェクトの生成（htmlにて設定したidをセット）
-const map = new geolonia.Map("#map");
-const geojson = "https://geolonia.github.io/style-demo-source/takamatsu-car-parking.json";
+// // mapオブジェクトの生成（htmlにて設定したidをセット）
+// const map = new geolonia.Map("#map");
+// const geojson = "https://geolonia.github.io/style-demo-source/takamatsu-car-parking.json";
 
-(async () => {
-    // 
-    const resJson = await fetch(geojson);
+// (async () => {
+//     // 
+//     const resJson = await fetch(geojson);
 
-    // フェッチに失敗したら
-    if (!resJson.ok) {
-        console.log("Error: GeoJSON not found.");
-        return;
-    }
+//     // フェッチに失敗したら
+//     if (!resJson.ok) {
+//         console.log("Error: GeoJSON not found.");
+//         return;
+//     }
 
-    const data = await resJson.json();
+//     const data = await resJson.json();
 
-    // Marderオブジェクトのセット
-    const setMarker = feature => {
-        const popup = new geolonia.Popup() // add popups
-        .setText(feature.properties["title"]);
+//     // Marderオブジェクトのセット
+//     const setMarker = feature => {
+//         const popup = new geolonia.Popup() // add popups
+//         .setText(feature.properties["title"]);
 
-        let options = {}
-        if ("24:00" !== feature.properties["endTime"]) {
-        options = {color: '#F9BF3D'};
-        }
+//         let options = {}
+//         if ("24:00" !== feature.properties["endTime"]) {
+//         options = {color: '#F9BF3D'};
+//         }
 
-        const marker = new geolonia.Marker(options)
-        .setLngLat(feature.geometry.coordinates)
-        .setPopup(popup)
-        .addTo(map);
+//         const marker = new geolonia.Marker(options)
+//         .setLngLat(feature.geometry.coordinates)
+//         .setPopup(popup)
+//         .addTo(map);
         
-        marker.getElement().style.cursor = 'pointer'
-    };
+//         marker.getElement().style.cursor = 'pointer'
+//     };
     
-    // Sorts features from North to South.
-    data.features.sort((a, b) => {
-        const { y: ay } = map.project(a.geometry.coordinates)
-        const { y: by } = map.project(b.geometry.coordinates)
-        return ay - by
-    })
+//     // Sorts features from North to South.
+//     data.features.sort((a, b) => {
+//         const { y: ay } = map.project(a.geometry.coordinates)
+//         const { y: by } = map.project(b.geometry.coordinates)
+//         return ay - by
+//     })
 
-    // 取得データ分Markerオブジェクトを生成
-    for (let i = 0; i < data.features.length; i++) {
-        setMarker(data.features[i])
-    }
+//     // 取得データ分Markerオブジェクトを生成
+//     for (let i = 0; i < data.features.length; i++) {
+//         setMarker(data.features[i])
+//     }
 
-})();
+// })();
